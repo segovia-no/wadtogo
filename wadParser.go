2a0deb0118231f6c6bd2d74bf3a012be275bb743
Diego Segovia <dsegoviaolivares@gmail.com>
2023-01-29 15:37:23 -0300

    WAD Directory parsing

--- wadParser.go
+++ wadParser.go
@@ -4,24 +4,67 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 )
 
 
-type WADParser struct {}
+type WADParser struct {
+	byteReader *bytes.Reader
+}
 
-func (WADParser) readHeaderData(b []byte) WADHeader {
 
-	byteReader := bytes.NewReader(b)
+type WADHeader struct {
+	WadType [4]byte
+	DirectoryEntries uint32
+	DirectoryOffset uint32
+}
+
+func (wp *WADParser) setupByteReader(b []byte) {
+	wp.byteReader = bytes.NewReader(b)
+}
+
+func (wp *WADParser) readHeaderData() WADHeader {
+	wp.checkValidByteReader()
 	
 	var wadHeader WADHeader
-	err := binary.Read(byteReader, binary.LittleEndian, &wadHeader)
+	err := binary.Read(wp.byteReader, binary.LittleEndian, &wadHeader)
 
 	if err != nil {
-		fmt.Println("[Error]: Invalid data when reading the WAD Header ", err)
+		fmt.Println("[Error]: Invalid data when reading the WAD Header:", err)
 		os.Exit(1)
 	}
 
 	return wadHeader
+}
 
+
+type DirectoryData struct {
+	LumpOffset uint32
+	LumpSize uint32
+	LumpName [8]byte
+}
+
+func (wp *WADParser) readDirectoryData(seekAt int64) DirectoryData {
+	wp.checkValidByteReader()
+
+	wp.byteReader.Seek(seekAt, io.SeekStart)
+	
+	var directoryData DirectoryData
+	err := binary.Read(wp.byteReader, binary.LittleEndian, &directoryData)
+
+	if err != nil {
+		fmt.Println("[Error]: Invalid data when reading WADs directory data:", err)
+		os.Exit(1)
+	}
+
+	return directoryData
+}
+
+
+func (wp *WADParser) checkValidByteReader() {
+	if (wp.byteReader == nil) {
+		fmt.Println("[Error]: No available byte reader, invoke it using setupByteReader(). Aborting execution")
+		os.Exit(1)
+	}
 }
--- wadloader.go
+++ wadloader.go
@@ -5,21 +5,53 @@ import (
 	"os"
 )
 
-var wadParser WADParser
+var wp WADParser
 
-type WADLoader struct {}
+type WADLoader struct {
+	WADBuffer []byte
+	WADFilename string
+	WADHeader WADHeader
+	WADDirectories WADDirectories
+}
 
-func (WADLoader) openAndLoad(wadFilename string) {
+func (wl *WADLoader) openAndLoad(wadFilename string) {
 
-	wadData, readErr := os.ReadFile(wadFilename)
+	wl.WADFilename = wadFilename
+
+	wadBuffer, readErr := os.ReadFile(wl.WADFilename)
 	if readErr != nil {
-		fmt.Println("[Error]: Couldn't read the WAD file")
+		fmt.Println("[Error]: Couldn't read the WAD file", readErr)
 		os.Exit(1)
 	}
 
-	wadHeader := wadParser.readHeaderData(wadData)
-	wadType := string(wadHeader.WadType[:])
+	wl.WADBuffer = wadBuffer
 
-	fmt.Println(wadType)
+	wp.setupByteReader(wl.WADBuffer)
+	wl.WADHeader = wp.readHeaderData()
 
+	fmt.Println("WAD Type:", string(wl.WADHeader.WadType[:]))
+	fmt.Println("WAD Dirs:", wl.WADHeader.DirectoryEntries)
+	fmt.Println("WAD Dir offset:", wl.WADHeader.DirectoryOffset)
+
+	wl.readWADDirectories()
+	fmt.Println(wl.WADDirectories[0])
+
+}
+
+type WADDirectories []DirectoryData
+
+func (wl *WADLoader) readWADDirectories() {
+
+	if wl.WADBuffer == nil || wl.WADHeader.DirectoryEntries < 1 {
+		fmt.Println("[Error]: Insufficient data to read WAD Directories")
+		os.Exit(0)
+	}
+
+	dirOffset := int64(wl.WADHeader.DirectoryOffset)
+
+	for i := 0; i < int(wl.WADHeader.DirectoryEntries); i++ {
+		dirData := wp.readDirectoryData(dirOffset + int64(i*16))
+		wl.WADDirectories = append(wl.WADDirectories, dirData)
+	}
+	
 }
