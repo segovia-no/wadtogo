8ef877fbcdca43244dc71cbc0a487a517a255972
Diego Segovia <dsegoviaolivares@gmail.com>
2023-01-29 17:01:23 -0300

    feat: Dump lumps to text file

---
+++ dumpLumps.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func dumpLumpsToTextFile(filename string, lumps WADDirectories) {
+
+	os.Remove(filename)
+	file, err := os.Create(filename)
+
+	if err != nil {
+		fmt.Println("[Error] Cannot create a file called", filename, err)
+		return
+	}
+
+	defer file.Close()
+
+	var errWrite error
+
+	_, errWrite = file.WriteString("Lump name | Size\n")
+
+	for _, lump := range lumps {
+
+		lumpName := bytes.Trim(lump.LumpName[:], "\x00")
+
+		outStr := fmt.Sprintf("%s %v \n", lumpName, lump.LumpSize)
+		_, errWrite = file.WriteString(outStr)
+	}
+
+	if errWrite != nil {
+		fmt.Println("[Error] Cannot add lump data to dump file", filename, err)
+		return
+	}
+
+}
